7e94ab7a3de767a0f215b46cbace06b67e4963e2
SamAugust69 <hellozqfb@protonmail.com>
2023-05-04 21:02:33 -0400

    fart

--- src/app/components/TopCards.tsx
+++ src/app/components/TopCards.tsx
@@ -19,9 +19,9 @@ const TopCards: FC<TopCardsProps> = ({activeLog, setActiveLog, logs}) => {
                 <Heading size="sm" className='font-normal text-slate-600 dark:text-slate-400 text-left'>Total Logs</Heading>
             </div>
             <div className='border-2 rounded border-slate-400 dark:border-slate-800 bg-slate-200 dark:bg-slate-600 px-6 py-2 w-full flex justify-between gap-2'>
-                {logs.map((val: any) => {
+                {logs.map((val: any, key: number) => {
                     return (
-                        <div className='rounded p-2 w-1/2 flex items-center justify-center flex-col relative cursor-pointer' onClick={() => setActiveLog(val.id)}>
+                        <div key={key} className='rounded p-2 w-1/2 flex items-center justify-center flex-col relative cursor-pointer' onClick={() => setActiveLog(val.id)}>
                             {activeLog === val.id && (
                                 <motion.div 
                                 className='bg-slate-300 w-full h-full absolute'
