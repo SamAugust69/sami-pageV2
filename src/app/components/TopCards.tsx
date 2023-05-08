1769bc1255ae12283b0c5da2462a1563216e293c
SamAugust69 <hellozqfb@protonmail.com>
2023-05-08 10:19:08 -0400

    ahh

--- src/app/components/TopCards.tsx
+++ src/app/components/TopCards.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { FC, useState } from 'react'
+
+import { FC, useState, useEffect } from 'react'
 import Paragraph from '@/ui/Paragraph'
 import Heading from '@/ui/Heading'
 import { motion } from 'framer-motion'
@@ -12,6 +12,7 @@ interface TopCardsProps {
 
 
 const TopCards: FC<TopCardsProps> = ({activeLog, setActiveLog, logs}) => {
+
     const [ totalLogs, setTotalLogs ] = useState(0)
     return (
         <div className='grid gap-4 lg:grid-cols-2' >
@@ -21,7 +22,11 @@ const TopCards: FC<TopCardsProps> = ({activeLog, setActiveLog, logs}) => {
             </div>
             <div className='border-2 rounded border-slate-400 dark:border-slate-800 bg-slate-200 dark:bg-slate-600 px-6 py-2 w-full flex justify-between gap-2 shadow-md'>
                 {logs.map((val: any, key: number) => {
-                    // setTotalLogs(totalLogs + val.amount.length)
+                    const [ amount, setAmoumt] = useState(0)
+
+                    useEffect(() => {
+                        setAmoumt(val.amount.length)
+                    }, [])
                     return (
                         <div key={key} className='rounded p-2 w-1/2 flex items-center justify-center flex-col relative cursor-pointer' onClick={() => setActiveLog(val.id)}>
                             {activeLog === val.id && (
@@ -31,7 +36,7 @@ const TopCards: FC<TopCardsProps> = ({activeLog, setActiveLog, logs}) => {
                                 />
                             )}
                             <Heading size="xs" className='font-normal text-slate-500 relative z-10'>{val.label}</Heading>
-                            <p className='font-normal text-xl relative z-10'>{val.amount[0] && val.amount.length}</p>
+                            <p className='font-normal text-xl relative z-10'>{amount}</p>
                         </div>
                     )
                 })}
