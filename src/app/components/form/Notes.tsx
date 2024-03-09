50b80649f4d517abc777b44781dec0ab0ad1b18b
Sam <sammonsonsonis@gmail.com>
2024-03-09 12:24:06 -0500

    test

--- src/app/components/form/Notes.tsx
+++ src/app/components/form/Notes.tsx
@@ -2,6 +2,7 @@ import { FormInputType, FormItems } from '@/lib/formTypes';
 import FormInput from '@/ui/FormInput';
 import Heading from '../ui/Heading';
 import Paragraph from '../ui/Paragraph';
+import { useEffect, useRef } from 'react';
 
 type stepItems = FormItems & {
 	updateForm: (item: Partial<FormItems>) => Promise<void>;
@@ -29,13 +30,20 @@ type stepItems = FormItems & {
 
 const Notes = ({ updateForm, notes }: stepItems) => {
 
+	
+	useEffect(() => {
+		textbox.current.value = notes
+	}, [notes])
+
+	const textbox = useRef<any>(<div></div>)
+
 	return (
 		<div>
 			<Heading size={'uberSmall'} className="text-t-100 my-1">
 				Notes About Robot (Optional)
 			</Heading>
 			<div className="py-2 flex flex-col gap-2">
-                <textarea onChange={(e: any) => updateForm({notes: e.target.value})} className='p-2 bg-transparent border-2 border-t-100 focus:ring-0 rounded text-t-100' />
+                <textarea ref={textbox} onChange={(e: any) => updateForm({notes: e.target.value})} className='p-2 bg-transparent border-2 border-t-100 focus:ring-0 rounded text-t-100' />
 			</div>
 		</div>
 	);
